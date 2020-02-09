8e48ef764fab19a71988f316f85fd775af626593
Derek <nightwing917@hotmail.com>
2020-02-08 20:03:45 -0700

    ready to install express

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 // console.log("I am executed immediately.");
 // module.exports = "I am the export for the router file!";
 
+// Installing express.js after upgrading to Windows 10 Trying to get everything back!
+
 const express = require('express');
 const router = express.Router();
 const userController = require('./controllers/userController');
