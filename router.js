9474bedfc738ad00703dc982dc899523592d1b6f
Derek <nightwing917@hotmail.com>
2020-02-09 20:32:32 -0700

    added /register to form

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 // console.log("I am executed immediately.");
 // module.exports = "I am the export for the router file!";
 
-// Installing express.js after upgrading to Windows 10 Trying to get everything back!
+
 
 const express = require('express');
 const router = express.Router();
