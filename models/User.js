bab39830418a1c41b2b2ad8e3796b2cecc1302de
Derek <nightwing917@hotmail.com>
2020-02-17 19:48:41 -0700

    Set up prototype for jump

--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
-// Constructor function
+// Constructor function - Blue Print Object
 let User = function() {
-
+    this.homePlanet = "earth";    
 }
+
+// add a method to constructor above
+User.prototype.jump = function() {};
 
 module.exports = User;
