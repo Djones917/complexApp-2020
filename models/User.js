a170e48643ac63ef6d83dbf716227497a71d9ba7
Derek <nightwing917@hotmail.com>
2020-02-19 19:58:25 -0700

    added all comments for setting up user data

--- models/User.js
+++ models/User.js
@@ -5,7 +5,11 @@ let User = function(data) {
 
 // add a method to constructor above
 User.prototype.register = function() {
-    // Step 1. validate User Data
+    // Step 1: validate user data
+
+    // Step 2: only if there are no validation errors
+
+    // Step 3: then save the user data into database
 };
 
 module.exports = User;
