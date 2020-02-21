d49581026abdefadf70d4623c07e22e26c205ca3
Derek <nightwing917@hotmail.com>
2020-02-20 19:37:46 -0700

    added this.errors

--- models/User.js
+++ models/User.js
@@ -1,18 +1,21 @@
 // Constructor function - Blue Print Object
 let User = function(data) {
     this.data = data;
+    this.errors = [];
 }
 
 
 User.prototype.validate = function() {
-    
+   if(this.data.username == "") {
+
+   }
 }
 
 
 // add a method to constructor above
 User.prototype.register = function() {
     // Step 1: validate user data
-
+    this.validate();
     // Step 2: only if there are no validation errors
 
     // Step 3: then save the user data into database
