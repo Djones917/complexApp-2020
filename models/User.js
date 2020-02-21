9914e4461daac035104e3d18ffa0da72c5198712
Derek <nightwing917@hotmail.com>
2020-02-20 19:52:41 -0700

    added username email and password to errors.push

--- models/User.js
+++ models/User.js
@@ -6,9 +6,9 @@ let User = function(data) {
 
 
 User.prototype.validate = function() {
-   if(this.data.username == "") {
-
-   }
+   if(this.data.username == "") {this.errors.push("You must provide a username!")}
+   if(this.data.email == "") {this.errors.push("You must provide a valid email address!")}
+   if(this.data.password == "") {this.errors.push("You must provide a valid password!")}
 }
 
 
