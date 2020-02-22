b5c258cdfa3ad7efaba9c320c5f244af78a6f4c2
Derek <nightwing917@hotmail.com>
2020-02-21 19:24:28 -0700

    added if statement password can not be longer than 100

--- models/User.js
+++ models/User.js
@@ -9,6 +9,8 @@ User.prototype.validate = function() {
    if(this.data.username == "") {this.errors.push("You must provide a username!")}
    if(this.data.email == "") {this.errors.push("You must provide a valid email address!")}
    if(this.data.password == "") {this.errors.push("You must provide a valid password!")}
+   if(this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must have atleast 12 charaters!")}
+   if(this.data.password.length > 100) {this.errors.push("Password can not exceed more than 100 charaters!")}
 }
 
 
