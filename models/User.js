80ec74d98ea0002b2f22f840668ac9ea171f5021
Derek <nightwing917@hotmail.com>
2020-02-24 19:25:22 -0700

    updated username

--- models/User.js
+++ models/User.js
@@ -11,8 +11,8 @@ User.prototype.validate = function() {
    if(this.data.password == "") {this.errors.push("You must provide a valid password!")}
    if(this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must have atleast 12 charaters!")}
    if(this.data.password.length > 100) {this.errors.push("Password can not exceed more than 100 charaters!")}
-   if(this.data.username.length > 0 && this.data.username.length < 12) {this.errors.push("Username must have atleast 12 charaters!")}
-   if(this.data.username.length > 100) {this.errors.push("Username can not exceed more than 100 charaters!")}
+   if(this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must have atleast 3 charaters!")}
+   if(this.data.username.length > 30) {this.errors.push("Username can not exceed more than 30 charaters!")}
 }
 
 
