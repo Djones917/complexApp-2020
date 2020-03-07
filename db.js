35667e6dbaf00e8cc613c9dba158328da3e2bacc
Derek <nightwing917@hotmail.com>
2020-03-06 19:47:49 -0700

    might have to change package.json on watch command

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mongodb = require('mongodb');
 
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+const connectionString = 'mongodb+srv://wishListApp:12345@cluster0-kccxp.mongodb.net/ComplexApp?retryWrites=true&w=majority';
 
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+   module.exports = client.db();
+   const app = require('./app');
+   app.listen(3000);
 });
+
