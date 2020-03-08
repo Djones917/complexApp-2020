4029c6fb79895fc04ea2205caa96ed5dbcc412dc
Derek <nightwing917@hotmail.com>
2020-03-07 20:38:39 -0700

    added CONNECTIONSTRING to mongodb.connect

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
+// npm install dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+
 const mongodb = require('mongodb');
 
-const connectionString = 'mongodb+srv://wishListApp:12345@cluster0-kccxp.mongodb.net/ComplexApp?retryWrites=true&w=majority';
-
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
    module.exports = client.db();
    const app = require('./app');
    app.listen(3000);
