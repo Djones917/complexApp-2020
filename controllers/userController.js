5c3380a66f57d13eebc754fdce0527c192254cd7
Derek <nightwing917@hotmail.com>
2020-04-14 20:38:47 -0600

    using then and catch methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,7 @@ const User = require('../models/User');
 
 exports.login = function(req, res) {
    let user = new User(req.body);
-   user.login(function(result) {
-     res.send(result);
-   });
+   user.login().then().catch();
 }
 
 
